7ba2b72c524ef3f0edbbc3be68d722e169fdc997
jarevrygo <2512330342@qq.com>
2017-05-17 22:44:11 +0800

    jsReviews1.js

---
+++ jsReview1.js
@@ -0,0 +1,25 @@
+var str = "the batter hit the ball with the bat";
+var re = /the/g;
+
+// console.log(str.charAt(0))
+// console.log(str.charCodeAt(0))
+
+// console.log(str.concat('wb','ln'))
+
+// console.log(str.split(" ",2))
+
+// console.log(str.slice(2,-2))
+// console.log(str.substr(-10))
+// console.log(str.substring(-10))
+
+// console.log(str.indexOf('Ev2'))
+// console.log(str.lastIndexOf('Ev'))
+// console.log(str.match(/E[t-z]/g))
+// console.log(str.search('w'))
+// console.log(str.replace(re,"&"))
+
+// console.log(str.toString())
+// console.log(str.valueOf())
+// console.log(str.trim())
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
